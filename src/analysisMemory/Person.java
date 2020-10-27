4f38b86cc362bf94b98c93f51658f9e249392324
曹先森 <1669928567@qq.com>
2020-10-27 09:54:55 +0800

    java举例解释new对象的内存分析,1027

---
+++ src/analysisMemory/Person.java
@@ -0,0 +1,10 @@
+package analysisMemory;
+
+public class Person {
+	int id;
+	int age;
+	Person(int x, int y){
+		id = x;
+		age = y;
+	}
+}
---
+++ src/analysisMemory/Test.java
@@ -0,0 +1,10 @@
+package analysisMemory;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person test01 = new Person(1001,15);
+		Person test02 = new Person(1002,14);
+	}
+
+}
